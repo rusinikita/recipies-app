68c37224dc14490fe7704aec235228a31bab698d
Nikita <rusinikit@gmail.com>
2017-11-05 21:36:22 +0200

    Add different images for recipes

--- app/src/main/java/com/nikita/recipiesapp/common/models/Recipe.java
+++ app/src/main/java/com/nikita/recipiesapp/common/models/Recipe.java
@@ -26,7 +26,26 @@ public final class Recipe {
   }
 
   public String getImageUri() {
-    return (image != null && !image.isEmpty()) ? image : "https://i0.wp.com/bakingamoment.com/wp-content/uploads/2015/03/4255featured2.jpg?w=720";
+    if (image != null && !image.isEmpty()) return image;
+
+    String url;
+    switch (id) {
+      case 1:
+        url = "https://i0.wp.com/bakingamoment.com/wp-content/uploads/2015/03/4255featured2.jpg?w=720";
+        break;
+      case 2:
+        url = "https://d2gk7xgygi98cy.cloudfront.net/4-3-large.jpg";
+        break;
+      case 3:
+        url = "https://dessertswithbenefits.com/wp-content/uploads/2014/01/33.jpg";
+        break;
+      case 4:
+        url = "http://img.taste.com.au/3kqM4rqu/w720-h480-cfill-q80/taste/2016/11/classic-baked-vanilla-cheesecake-53202-1.jpeg";
+        break;
+      default:
+        url = "http://img.taste.com.au/BcemwIdD/taste/2016/11/chocolate-celebration-cake-85607-1.jpeg";
+    }
+    return url;
   }
 
   @Override
